e40f62a954543fae5fea2f3b81d4b70d0518efaa
Follet Victor <follet.victor@gmail.com>
2013-01-15 02:47:19 +0400

    Iteration F3: Highlighting Changes

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,11 +1,9 @@
-#!/bin/env ruby
-# encoding: utf-8
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
-  attr_accessible :cart_id, :product_id, :price
+  attr_accessible :cart_id, :product_id
 
   def total_price
-        price * quantity
+    price * quantity
   end
 end
